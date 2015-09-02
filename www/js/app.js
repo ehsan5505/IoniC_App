47442ccd8ac85f11cdc3795f97f9d340743ef82b
Ehsan Rafeeque <dreamer_2142@yahoo.com>
2015-09-02 07:37:37 +0000

    adding navigation to the system

--- www/js/app.js
+++ www/js/app.js
@@ -17,6 +17,26 @@ angular.module('starter', ['ionic'])
     }
   })
 })
+
+.config(function($stateProvider,$urlRouterProvider){
+  $stateProvider
+    .state('tabs',{
+      url:'/tab',
+      abstract: true,
+      templateUrl:'template/tabs.html',
+    })
+    .state('tabs.persons',{
+      url:'/person',
+      views: {
+        'list-person': {
+          templateUrl: 'template/student.html',
+          controller: 'ListController'
+        }
+      }
+    })
+  $urlRouterProvider.otherwise('/tab/person');
+})
+
 .controller('ListController',['$scope','$http',controlFunctionality]);
 
 function controlFunctionality($scope,$http){
